b15d8f8fdf49c4e8eaa3eabce4b4a0093a6bc414
Erin Hollett <erinkhollett@gmail.com>
2025-08-02 22:37:37 -0400

    Add edit post button functionality

--- new-post/new-post.js
+++ new-post/new-post.js
@@ -1,36 +1,47 @@
 window.addEventListener('DOMContentLoaded', function() {
   const formElement = document.getElementById('blogForm');
-  formElement.addEventListener("submit", function(event) {
-    event.preventDefault();
+  console.log('how many times does this print/')
 
-    const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
-    const image = document.getElementById('image').value;
-
-    // form validation to make sure the title and content isn't empty
-    if (title === "" || content === "") {
-      alert("Post not completed. Please fill in the title and content before submitting.");
-    } else { 
-
-      // making the post object with data from the inputted form values
-      const postData = {
-        title: title,
-        content: content,
-        image: image,
-        date: Date.now(),
-        id: Date.now().toString(),
-      };
-
-      // == LOCAL STORAGE == //
-
-      // retrieve current array of postData from localStorage
-      // it parses through the JSON first to turn it from a string to an array
-      let posts = JSON.parse(localStorage.getItem('myPosts'));
-      if (!posts) {
-        posts = [];
-      }
-      posts.push(postData);  // push the new postData to the localStorage array
-      localStorage.setItem('myPosts', JSON.stringify(posts)); // save updated to local storage again (as a JSON string)
-    }
-  });
+  if (window.location.search.includes('?id=')) {
+    const postId = window.location.search.split('=')[1];
+    const post = getPost(postId);
+    console.log(post);
+    document.getElementById('title').value = post.title;
+    document.getElementById('content').value = post.content;
+    document.getElementById('image').value = post.image;
+  }
+  formElement.addEventListener("submit", submitPost);
 });
+
+function submitPost(event) {
+  event.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const content = document.getElementById('content').value;
+  const image = document.getElementById('image').value;
+
+  // form validation to make sure the title and content isn't empty
+  if (title === "" || content === "") {
+    alert("Post not completed. Please fill in the title and content before submitting.");
+  } else { 
+    // get or make new id
+    let postId = Date.now().toString();
+    if (window.location.search.includes('?id=')) {
+      postId = window.location.search.split('=')[1];
+    }
+
+    // making the post object with data from the inputted form values
+    const postData = {
+      title: title,
+      content: content,
+      image: image,
+      date: Date.now(),
+      id: postId,
+    };
+
+    savePost(postData);
+
+    // redirect to new post!
+    window.location.href = `../post/post.html?id=${postData.id}`;
+  }
+}
